34f08f3148d97335560aa8204d99c62501e6e5cc
alenaiwaly <79890759+alenaiwaly@users.noreply.github.com>
2021-03-15 12:31:12 +0800

    Add files via upload

---
+++ dz2 исправленное/dz2/FlashMedia.java
@@ -0,0 +1,11 @@
+
+package dz2;
+
+public class FlashMedia extends Media {
+    /**
+     * Конструктор суперкласса с параметром
+     */
+    public FlashMedia(){
+        super("Флэш-носитель");
+    }
+}
